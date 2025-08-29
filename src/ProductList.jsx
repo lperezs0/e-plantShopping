f43034032dd74e656bfb899796e37e6fd924704f
Luis Perez <prez.luis93@gmail.com>
2025-08-29 00:59:09 -0400

    task 1 done

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import CartSlice, { addItem } from './CartSlice';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({}); //to track which products added to the cart
 
     const plantsArray = [
         {
@@ -252,6 +255,12 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+    const handleAddToCart = (product) => {
+        dispatchEvent(addItem(product));
+        setAddedToCart((prevState) => ({ ...prevState, [product.name]: true,
+
+        }));
+    };
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -272,10 +281,33 @@ function ProductList({ onHomeClick }) {
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
-            {!showCart ? (
-                <div className="product-grid">
+            {!showCart ? ( //display the array plantsArray details, name, image, description and cost.
+                <div className="product-grid"> 
+                    {plantsArray.map((category, index) => ( // loop all the catories in the array
+                        <div key={index}> 
+                            <h1><div>{category.category}</div></h1>
+                            <div className="product-list">
+                                {category.plants.map((plant, plantIndex)=> (
+                                <div className="product-card" key={plantIndex}>
+                                    <img className="product-image"
+                                    src={plant.image} //displaying the images of the plants
+                                    alt={plant.name}
+                                    />
+                                    {/*display the plant name, description and cost */}
+                                    <div className="product-title">{plant.name}</div> 
+                                    <div className="product-description">{plant.description}</div>
+                                    <div className="product-cost">${plant.cost}</div>
+                                    {/*display the add to cart button for each plant */}
+                                    <button className="product-button" onClick={() => handleAddToCart(plant)}>
+                                        Add to Cart
+                                    </button>
+                                        
+                                </div>
+                            ))}
 
-
+                            </div>
+                        </div>
+                    ))}
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
