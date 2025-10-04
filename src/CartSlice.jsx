023aad3cc73b2cc0cb5f38e30dbb08fd94e60898
Luis Perez <prez.luis93@gmail.com>
2025-10-03 23:47:33 -0400

    task 4 done

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -17,7 +17,7 @@ export const CartSlice = createSlice({
          }
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !==action.payload);
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
         const {name, quantity} = action.payload;
