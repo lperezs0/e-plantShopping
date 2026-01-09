0c79b6c8ddbd80deb3f8c84d65a6d077f6f6c893
Luis Perez <prez.luis93@gmail.com>
2026-01-08 23:48:12 -0500

    cart item functions

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,43 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
-  };
-
-  const handleContinueShopping = (e) => {
-   
-  };
-
-
-
+    return cart.reduce((total, item) => total + item.cost * item.quantity, 0);
+};
+  /*let total = 0;
+  cart.forEach(item => {
+      const quantity = item.quantity;
+      const costString = item.cost;
+      const cost = parseFloat(costString.substring(1));
+      const itemTotal = cost * quantity;
+      total += itemTotal;
+  });
+  return total;
+};*/
+  
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity +1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1){
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+   }
+   else {
+        dispatch(removeItem(item.name));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.cost * item.quantity;
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
